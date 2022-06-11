aadd8817e2142e7d0f4690010ca1e17ccb4a53dc
FredSV17 <fredvillar64@gmail.com>
2022-06-10 21:11:08 -0300

    Adição do pré-processamento do banco de dados Adição do algoritmo de previsão - Árvore de decisão

--- utils.py
+++ utils.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 #Função para remover os limitadores default do jupyter notebook
 def remove_limiters():
+    print("Setting limiters to none")
     pd.set_option('display.max_columns', None)  # or 1000
     pd.set_option('display.max_rows', None)  # or 1000
     pd.set_option('display.max_colwidth', None)  # or 199
-    print("Funciona")
+
 
 def one_hot(database, column):
     onehot = pd.get_dummies(database[column])
@@ -14,3 +15,6 @@ def one_hot(database, column):
     concat_data = concat_data.drop(column,axis=1)
     return concat_data
 
+def group_data(database, column):
+    database.groupby(column).mean()
+    return database
